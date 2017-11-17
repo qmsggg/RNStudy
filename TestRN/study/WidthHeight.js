6ff8a6f21714e10e25813e77ef0cd7b79a539d0b
罗浩 <luohao0414@gmail.com>
2017-11-18 03:47:52 +0800

    FlexDirection：row

--- TestRN/study/WidthHeight.js
+++ TestRN/study/WidthHeight.js
@@ -4,14 +4,12 @@ import { View } from 'react-native';
 export default class FixedDimensBasics extends Component<{}> {
     render() {
         return (
-            // 试试去掉父View中的`flex: 1`。
-            // 则父View不再具有尺寸，因此子组件也无法再撑开。
-            // 然后再用`height: 300`来代替父View的`flex: 1`试试看？
-            <View style={{ height:300 }}>
-                <View style={{flex: 1, backgroundColor: 'powderblue'}}></View>
-                <View style={{flex: 2, backgroundColor: 'skyblue'}}></View>
-                <View style={{flex: 3, backgroundColor: 'steelblue'}}></View>            
-            </View>
+            // 尝试把`flexDirection`改为`column`看看
+        <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+            <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+        </View>
         );
     }
 };
