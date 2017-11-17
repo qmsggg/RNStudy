f68ebdd1aa4f37069349b1b3ba50dd7817580e52
罗浩 <luohao0414@gmail.com>
2017-11-17 13:48:00 +0800

    Props

---
+++ TestDemo/index.js
@@ -0,0 +1,19 @@
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+
+// AppRegistry.registerComponent('TestDemo', () => App);
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
+
+class Bananas extends Component {
+    render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
+        return (
+            <Image source={pic} style={{width: 193, height: 110}} />
+        );
+    }
+}
+
+AppRegistry.registerComponent('TestDemo', () => Bananas);
