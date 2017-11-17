8e0abb6314275394f1eae6a262f855f7c406e029
罗浩 <luohao0414@gmail.com>
2017-11-18 03:51:08 +0800

    space-between

--- TestRN/study/WidthHeight.js
+++ TestRN/study/WidthHeight.js
@@ -4,12 +4,17 @@ import { View } from 'react-native';
 export default class FixedDimensBasics extends Component<{}> {
     render() {
         return (
-            // 尝试把`flexDirection`改为`column`看看
-        <View style={{flex: 1, flexDirection: 'row'}}>
-            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-            <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-        </View>
+        // 尝试把`justifyContent`改为`center`看看
+      // 尝试把`flexDirection`改为`row`看看
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+      </View>
         );
     }
 };
